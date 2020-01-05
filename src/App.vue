d24ede9fe0c0da2ce1730015808b217999b32080
Dengsuning <deng_suning@163.com>
2020-01-05 23:56:13 +0800

    favor:homeNavigator

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,34 @@
 <template>
   <div id="app">
     <div id="nav">
-      <!-- 前端 后端 工具 面试 demo -->
-      <section>
-        <h1 class="white-shadow">Focus On</h1>
-        <p class="white-shadow">学习前端、后端笔记，专注成就自我</p>
-        <section class="home-card-wrapper">
-          <div class="home-card"></div>
-        </section>
-      </section>
+      <a-row>
+        <a-col :xs="{span:24}" :md="{span:22}">
+          <h1 class="white-shadow" style="color:white;">Focus On</h1>
+          <p class="white-shadow">前端、后端学习笔记，专注成就自我</p>
+        </a-col>
+      </a-row>
+      <a-row>
+        <a-col :xs="{span:20,offset:2}" :md="{span:20,offset:1}" :lg="{span:7,offset:1}">
+          <home-navigator></home-navigator>
+        </a-col>
+        <a-col :xs="{span:0}" :md="{span:0}" :lg="{span:12,offset:4}">
+          <router-view />
+        </a-col>
+      </a-row>
+
       <!-- <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>-->
     </div>
-    <router-view />
   </div>
 </template>
-
+<script>
+import HomeNavigator from '@/components/HomeNavigator'
+export default {
+  components: {
+    'home-navigator': HomeNavigator
+  }
+}
+</script>
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
@@ -23,6 +36,11 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+  height: 100%;
+}
+#container {
+  position: absolute;
 }
 
 #nav {
@@ -31,9 +49,8 @@
   top: 0;
   width: 100vw;
   height: 100vh;
-  background: rgba(220, 221, 216) url("~@/assets/bg.jpg") no-repeat;
-  background-position: 45% 0;
-  background-size: 400px 100%;
+  background: #ddd url("~@/assets/images/bg1.png") no-repeat;
+  background-position: 35% bottom;
   color: white;
 }
 
@@ -45,11 +62,13 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
-
-.home-card-wrapper {
+@media screen and (max-width: 959px) {
+  #nav {
+    background-position-x: center;
+  }
 }
 
-.white-shadow{
-  text-shadow: 0 0 4px rgb(255, 255, 255,.6);
+.white-shadow {
+  text-shadow: 0 0 4px rgb(255, 255, 255, 0.6);
 }
 </style>
---
+++ src/components/HomeNavigator.vue
@@ -0,0 +1,109 @@
+<template >
+  <!-- 前端 后端 工具 面试 demo -->
+  <section class="navigation">
+    <div class="home-card">
+      <img class="card-img" src="@/assets/images/1.png" alt="目录图片" />
+      <span>前端·点滴</span>
+    </div>
+    <div class="home-card">
+      <img class="card-img" src="@/assets/images/3.png" alt="目录图片" />
+      <span>后端·笔记</span>
+    </div>
+    <div class="home-card">
+      <img class="card-img" src="@/assets/images/2.png" alt="目录图片" />
+      <span>缘·工具</span>
+    </div>
+    <div class="home-card">
+      <img class="card-img" src="@/assets/images/7.png" alt="目录图片" />
+      <span>随心·DEMO</span>
+    </div>
+  </section>
+</template>
+<style lang="scss" scoped>
+.navigation {
+  margin-top: 5rem;
+  width: 100%;
+}
+
+.home-card {
+  left: 0;
+  height: 60px;
+  cursor: pointer;
+  max-width: 450px;
+  min-width: 278px;
+  line-height: 60px;
+  position: relative;
+  border-radius: 40px;
+  border: 1px solid white;
+  border-top-width: 0;
+  box-sizing: border-box;
+  background: rgba(255, 255, 255, 0.4);
+
+  &:first-child {
+    border-top-width: 1px;
+  }
+
+  &:nth-child(even) {
+    text-align: right;
+    direction: rtl;
+  }
+
+  &:nth-child(odd) {
+    text-align: left;
+  }
+
+  &:hover .card-img {
+    transform: rotate(2turn);
+    left: 20px;
+  }
+
+  > span {
+    vertical-align: text-bottom;
+    margin-left: 1rem;
+    margin-right: 1rem;
+    color: #303030;
+  }
+}
+
+.card-img {
+  width: 60px;
+  height: 60px;
+  vertical-align: middle;
+  border-radius: 30px;
+  background: #f5f5f5;
+  padding: 8px;
+  transition: transform 4s, left 4s, -webkit-transform 4s;
+}
+
+@media screen and (max-width: 959px) {
+  .navigation .home-card {
+    width: 100%;
+    max-width: 100%;
+  }
+}
+
+/* .home-card::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 60px;
+  background: white;
+  transition: width 2s;
+  border-radius: 60px;
+  z-index: -1
+} 
+.home-card:nth-child(odd)::after {
+  left: 0;
+}
+
+.home-card:nth-child(even)::after {
+  right: 0;
+}
+
+.home-card:hover::after {
+  width: 100%;
+}
+
+*/
+</style>
